84efc224e44f42b8e0f359ab110f9d82d40ea6f3
Pedro Monteiro <montteiropedro@gmail.com>
2023-05-05 17:05:02 -0300

    Testes unitários do modelo de produtos refatorado

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,109 +2,94 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe '#valid?' do
-    context 'presence validation' do
-      it 'should return false when code is empty' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
-        
+    context 'code' do
+      it 'should not be empty' do
         allow(SecureRandom).to receive(:alphanumeric).and_return('')
-        result = product.valid?
+        product = Product.new
         
-        expect(result).to eq false
-      end
-      
-      it 'should return false when name is empty' do
-        product = Product.new(
-          name: '', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
-
-        result = product.valid?
-
-        expect(result).to eq false
-      end
-    end
-
-    context 'uniqueness validation' do
-      it 'should return false when code is not unique' do
-        first_product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
-
-        second_product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
+        product.valid?
         
+        expect(product.errors.include? :code).to eq true
+        expect(product.errors[:code].include? 'não pode ficar em branco').to eq true
+      end
+
+      it 'should be unique' do
         allow(SecureRandom).to receive(:alphanumeric).and_return('ABCDEFGHIJ')
-        first_product.save!
+        first_product = Product.create!(
+          name: 'TV 32 Polegadas', weight: 1_000,
+          width: 100, height: 50, depth: 10
+        )
+        second_product = Product.new
+        
+        second_product.valid?
 
-        result = second_product.valid?
-
-        expect(result).to eq false
+        expect(second_product.errors.include? :code).to eq true
+        expect(second_product.errors[:code].include? 'já está em uso').to eq true
       end
-    end
 
-    context 'length validation' do
       it 'should return false when code length is not 10' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
+        allow(SecureRandom).to receive(:alphanumeric).and_return('ABC12')
+        product = Product.new
         
-        allow(SecureRandom).to receive(:alphanumeric).and_return('ABCDE')
-        result = product.valid?
+        product.valid?
         
-        expect(result).to eq false
+        expect(product.errors.include? :code).to eq true
+        expect(product.errors[:code].include? 'não possui o tamanho esperado (10 caracteres)').to eq true
       end
     end
 
-    context 'numericality validation' do
-      it 'should return false when weight is not an integer greater than 0' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: -1_000,
-          width: 100, height: 50, depth: 10
-        )
-        
-        result = product.valid?
-        
-        expect(result).to eq false
-      end
+    context 'name' do
+      it 'should not be empty' do
+        product = Product.new(name: '')
 
-      it 'should return false when width is not an integer greater than 0' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: -100, height: 50, depth: 10
-        )
-        
-        result = product.valid?
-        
-        expect(result).to eq false
-      end
+        product.valid?
 
-      it 'should return false when height is not an integer greater than 0' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: -50, depth: 10
-        )
-        
-        result = product.valid?
-        
-        expect(result).to eq false
+        expect(product.errors.include? :name).to eq true
+        expect(product.errors[:name].include? 'não pode ficar em branco').to eq true
       end
+    end
 
-      it 'should return false when depth is not an integer greater than 0' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: -10
-        )
+    context 'weight' do
+      it 'should be a integer greater than 0' do
+        product = Product.new(weight: -1)
         
-        result = product.valid?
+        product.valid?
         
-        expect(result).to eq false
+        expect(product.errors.include? :weight).to eq true
+        expect(product.errors[:weight].include? 'deve ser maior que 0').to eq true
+      end
+    end
+
+    context 'width' do
+      it 'should be a integer greater than 0' do
+        product = Product.new(width: -1)
+        
+        product.valid?
+        
+        expect(product.errors.include? :width).to eq true
+        expect(product.errors[:width].include? 'deve ser maior que 0').to eq true
+      end
+    end
+
+    context 'height' do
+      it 'should be a integer greater than 0' do
+        product = Product.new(height: -1)
+        
+        product.valid?
+        
+        expect(product.errors.include? :height).to eq true
+        expect(product.errors[:height].include? 'deve ser maior que 0').to eq true
+      end
+    end
+
+    context 'depth' do
+      it 'should be a integer greater than 0' do
+        product = Product.new(depth: -1)
+        
+        product.valid?
+        
+        expect(product.errors.include? :depth).to eq true
+        expect(product.errors[:depth].include? 'deve ser maior que 0').to eq true
       end
     end
   end
@@ -112,41 +97,27 @@ RSpec.describe Product, type: :model do
   describe '#set_alphanumeric_code' do
     context 'return a alphanumeric random code' do
       it 'should be made automatically' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
+        product = Product.new
 
         product.valid?
-        code = product.code
 
-        expect(code).to be_truthy
+        expect(product.code).to be_truthy
       end
 
-      it 'should have length equal to 10' do
-        product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
+      it 'should have size equal to 10' do
+        product = Product.new
 
         product.valid?
-        code_length = product.code.length
 
-        expect(code_length).to eq 10
+        expect(product.code.size).to eq 10
       end
 
       it 'should be unique' do
-        first_product = Product.create!(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
+        first_product = Product.new
+        second_product = Product.new
 
-        second_product = Product.new(
-          name: 'TV 32 Polegadas', weight: 1_000,
-          width: 100, height: 50, depth: 10
-        )
-
-        second_product.save!
+        first_product.valid?
+        second_product.valid?
 
         expect(second_product.code).not_to eq first_product.code
       end
