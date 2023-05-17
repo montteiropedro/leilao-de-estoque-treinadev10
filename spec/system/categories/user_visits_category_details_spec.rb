e2f8924faadcb99cada07b14124dbe7d95a8338b
Pedro Monteiro <montteiropedro@gmail.com>
2023-05-17 14:37:03 -0300

    Novos casos de teste de sistema adicionados na tela de detalhes de uma categoria e pequenas mudanças no CSS de algumas views

--- spec/system/categories/user_visits_category_details_spec.rb
+++ spec/system/categories/user_visits_category_details_spec.rb
@@ -17,24 +17,91 @@ describe 'User visits a categoy details page' do
     expect(page).to have_content 'Categorias'
   end
 
-  it 'should be successful' do
-    category = Category.create!(name: 'Eletrônicos')
+  context 'when successful' do
+    it 'should see links to products details page that are related to it' do
+      category = Category.create!(name: 'Jogos Eletrônicos')
+      Product.create!(
+        name: 'Uncharted 3', weight: 300,
+        width: 15, height: 30, depth: 5, category: category
+      )
+      Product.create!(
+        name: 'Ghost of Tsushima', weight: 300,
+        width: 15, height: 30, depth: 5, category: category
+      )
+      Product.create!(
+        name: 'Grand Theft Auto V', weight: 300,
+        width: 15, height: 30, depth: 5, category: category
+      )
 
-    visit root_path
-    click_on 'Listar Categorias'
-    click_on 'Eletrônicos'
+      visit root_path
+      click_on 'Listar Categorias'
+      click_on 'Jogos Eletrônicos'
 
-    expect(current_path).to eq category_path(category)
-    expect(page).to have_content 'Eletrônicos'
+      expect(current_path).to eq category_path(category)
+      expect(page).to have_content 'Jogos Eletrônicos'
+      expect(page).to have_content 'Uncharted 3'
+      expect(page).to have_content 'Ghost of Tsushima'
+      expect(page).to have_content 'Grand Theft Auto V'
+    end
+
+    it 'should see the products disponibility status' do
+      user = User.create!(
+        name: 'Peter Parker', cpf: '73046259026',
+        email: 'peter@email.com', password: 'password123'
+      )
+      first_admin_user = User.create!(
+        name: 'John Doe', cpf: '41760209031',
+        email: 'john@leilaodogalpao.com.br', password: 'password123'
+      )
+      second_admin_user = User.create!(
+        name: 'Steve Gates', cpf: '35933681024',
+        email: 'steve@leilaodogalpao.com.br', password: 'password123'
+      )
+      approved_lot = Lot.create!(
+        code: 'COD123456', start_date: Date.today, end_date: Date.today + 1.month,
+        min_bid_in_centavos: 10_000, min_diff_between_bids_in_centavos: 5_000,
+        creator: first_admin_user, approver: second_admin_user
+      )
+      sold_lot = Lot.new(
+        code: 'KDA334509', start_date: Date.today, end_date: Date.today + 1.day,
+        min_bid_in_centavos: 10_000, min_diff_between_bids_in_centavos: 3_000,
+        creator: first_admin_user, approver: second_admin_user, buyer: user
+      )
+      sold_lot.save!(validate: false)
+      category = Category.create!(name: 'Jogos Eletrônicos')
+
+      Product.create!(
+        name: 'Uncharted 3', weight: 300,
+        width: 15, height: 30, depth: 5, category: category
+      )
+      Product.create!(
+        name: 'Ghost of Tsushima', weight: 300,
+        width: 15, height: 30, depth: 5, category: category,
+        lot: approved_lot
+      )
+      Product.create!(
+        name: 'Grand Theft Auto V', weight: 300,
+        width: 15, height: 30, depth: 5, category: category,
+        lot: sold_lot
+      )
+
+      visit root_path
+      click_on 'Listar Categorias'
+      click_on 'Jogos Eletrônicos'
+
+      expect(page).to have_content 'Disponível'
+      expect(page).to have_content 'Indisponível'
+      expect(page).to have_content 'Vendido'
+    end
   end
 
   context 'and is a visitant' do
     it 'should not see the button to remove the category' do
-      category = Category.create!(name: 'Eletrônicos')
+      category = Category.create!(name: 'Jogos Eletrônicos')
 
       visit root_path
       click_on 'Listar Categorias'
-      click_on 'Eletrônicos'
+      click_on 'Jogos Eletrônicos'
   
       expect(page).not_to have_button 'Remover Categoria'
     end
@@ -46,12 +113,12 @@ describe 'User visits a categoy details page' do
         name: 'Peter Parker', cpf: '73046259026',
         email: 'peter@email.com', password: 'password123'
       )
-      category = Category.create!(name: 'Eletrônicos')
+      category = Category.create!(name: 'Jogos Eletrônicos')
 
       login_as(user)
       visit root_path
       click_on 'Listar Categorias'
-      click_on 'Eletrônicos'
+      click_on 'Jogos Eletrônicos'
   
       expect(page).not_to have_button 'Remover Categoria'
     end
@@ -63,12 +130,12 @@ describe 'User visits a categoy details page' do
         name: 'John Doe', cpf: '41760209031',
         email: 'john@leilaodogalpao.com.br', password: 'password123'
       )
-      category = Category.create!(name: 'Eletrônicos')
+      category = Category.create!(name: 'Jogos Eletrônicos')
 
       login_as(admin_user)
       visit root_path
       click_on 'Listar Categorias'
-      click_on 'Eletrônicos'
+      click_on 'Jogos Eletrônicos'
   
       expect(page).to have_button 'Remover Categoria'
     end
