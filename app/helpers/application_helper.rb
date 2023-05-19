0dc5f33b88c32a1b0d5f673743d5a117ee5a1bff
Pedro Monteiro <montteiropedro@gmail.com>
2023-05-18 23:46:31 -0300

    Extrai o helpers relacionados a conversão monetária do application_helper para um arquivo próprio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,2 @@
 module ApplicationHelper
-  def centavos_to_reais(amount, with_unit:)
-    if with_unit
-      number_to_currency(amount / 100, unit: 'R$', separator: ',', delimiter: '.')
-    else
-      number_to_currency(amount / 100, unit: '', separator: ',', delimiter: '.')
-    end
-  end
-
-  def format_cpf(text)
-    cpf = text.match(/(\d{3})(\d{3})(\d{3})(\d{2})/)
-
-    "#{cpf[1]}.#{cpf[2]}.#{cpf[3]}-#{cpf[4]}"
-  end
 end
